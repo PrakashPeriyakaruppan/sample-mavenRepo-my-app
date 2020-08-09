c00cfcd90324f5d9183311f4d05086a3f4178681
Prakash Periyakaruppan <prakashsethu@gmail.com>
2020-08-09 08:58:58 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,5 +10,7 @@ public class App
     {
         System.out.println( "Hello World updated for version 5!" );
             System.out.println( "Hello World updated for v7.0!" );
+              System.out.println( "Hello World updated for v8.0! 09082020 0900 am" );
+    }
     }
 }
